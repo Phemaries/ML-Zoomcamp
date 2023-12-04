336ed90a865166fa9030e71538a177a61809fff3
Phemaries <68478062+Phemaries@users.noreply.github.com>
2023-12-04 19:00:38 +0100

    Add files via upload

---
+++ Ml_Zoomcamp_Week_10/solutions.py
@@ -0,0 +1,4 @@
+# Question 1 => {'get_credit': True, 'get_credit_probability': 0.726936946355423}
+# Question 2 => kind version 0.20.0
+# Question 3 => 10.96.0.1 (Cluster IP using kubectl get services)
+# Question 4 => kind load docker-image
